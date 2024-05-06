e2120c5a45a6a126520aa8f7ed9db5cb7e128995
Arpan Patel <arpanptl.29@gmail.com>
2024-05-06 18:38:59 +0530

    Fix dark theme images

--- script.js
+++ script.js
@@ -8,9 +8,20 @@ themeButton.addEventListener("click", () => {
   if (themeToggle.dataset.theme === "dark") {
     themeToggle.dataset.theme = "light";
     themeToggle.innerHTML = "L";
+    document.getElementById("illustration").src =
+      "images/illustration-editor-mobile.svg";
+    document.getElementById("phones").src = "images/illustration-phones.svg";
+    document.getElementById("laptop").src =
+      "images/illustration-laptop-mobile.svg";
   } else {
     themeToggle.dataset.theme = "dark";
     themeToggle.innerHTML = "D";
+    document.getElementById("illustration").src =
+      "images/illustration-editor-mobile-dark.svg";
+    document.getElementById("phones").src =
+      "images/illustration-phones-dark.svg";
+    document.getElementById("laptop").src =
+      "images/illustration-laptop-mobile-dark.svg";
   }
 
   body.classList.toggle("darkTheme");
